b00c75d8ccbff45a92c6e3ea40411d34acf8fd00
Hongrui Fang <chfanghr@gmail.com>
2021-08-03 14:13:27 +0800

    format example source code

--- Sources/Example/main.swift
+++ Sources/Example/main.swift
@@ -8,28 +8,28 @@ import Foundation
 import Skoptsy
 
 let p = Module { name in
-    activity (name.Wait, [name.ticks]) { val in
+    activity(name.Wait, [name.ticks]) { val in
         exec { val.i = val.ticks as Int }
         `while` { val.i > 0 } repeat: {
             exec { val.i -= 1 }
             `await` { true }
         }
     }
-    activity (name.Main, []) { val in
+    activity(name.Main, []) { val in
         cobegin {
             strong {
-                run (name.Wait, [10])
+                run(name.Wait, [10])
             }
             weak {
                 `repeat` {
-                    run (name.Wait, [2])
+                    run(name.Wait, [2])
                     exec { print("on every third") }
                     `await` { true }
                 }
             }
             weak {
                 `repeat` {
-                    run (name.Wait, [1])
+                    run(name.Wait, [1])
                     exec { print("on every second") }
                     `await` { true }
                 }
@@ -39,9 +39,9 @@ let p = Module { name in
     }
 }.makeProcessor()!
 
-while true{
+while true {
     print("--> tick")
-    guard try p.tick([], []) != .done else{
+    guard try p.tick([], []) != .done else {
         break
     }
 }
